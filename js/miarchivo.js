e5550bec6dcc6ca6e756daca2e1f2906c79411c4
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2021-09-07 19:59:54 -0300

    Desafío clase 09 - Eventos - Agregué el boton de borrar producto y validaciones

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,7 +1,8 @@
 //DESAFÍO DOM CLASE 08
 
 class Productos{
-    constructor(nombre, precio, stock){
+    constructor(id, nombre, precio, stock){
+        this.id = id; 
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
@@ -12,9 +13,7 @@ let div_productos = document.getElementById('productos');
 let cantidad_productos = document.getElementById('cantidad_productos');
 
 let productosJSON;
-//console.log(localStorage.getItem('productos'));
-console.log(JSON.parse(localStorage.getItem('productos')));
-//ESTUVE MUCHO TIEMPO VIENDO COMO HACER PARA QUE TRAER LO GUARDADO EN EL LOCALSTORAGE, SE ME PISABAN LAS VARIABLES AL TOCAR F5 POR QUE LAS VOLVIA A DECLARAR, LES GUARDE EL VALOR DEL LOCALSTORAGE, SI NO HAY NADA EL VALOR ES NULL, EN ESE CASO SI DECLARAR LAS VARIALBES.
+//ESTUVE MUCHO TIEMPO VIENDO COMO HACER PARA TRAER LO GUARDADO EN EL LOCALSTORAGE, SE ME PISABAN LAS VARIABLES AL TOCAR F5 POR QUE LAS VOLVIA A DECLARAR, LES GUARDE EL VALOR DEL LOCALSTORAGE, SI NO HAY NADA EL VALOR ES NULL, EN ESE CASO SI DECLARAR LAS VARIALBES.
 let productos_guardados = JSON.parse(localStorage.getItem('productos'));
 let listado_productos = JSON.parse(localStorage.getItem('productos'));;
 if(productos_guardados == null){
@@ -24,35 +23,70 @@ if(productos_guardados == null){
     console.log(listado_productos);
     cantidad_productos.innerHTML = productos_guardados.length;
     for(producto of productos_guardados){
-        div_productos.innerHTML += `<ul class='productos__lista'>
+        div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
                                             <li class='productos__item'>NOMBRE: ${producto.nombre}</li>
                                             <li class='productos__item'>PRECIO: ${producto.precio}</li>
                                             <li class='productos__item'>STOCK: ${producto.stock}</li>
+                                            <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
                                        </ul>`
     }
 }
-
 function agregarProducto(){
         let nombre = document.getElementById('nombre').value;
         let precio = document.getElementById('precio').value;
         let stock = document.getElementById('stock').value;
-        let producto = new Productos(nombre, precio, stock);
+        let producto = new Productos(0 ,nombre, precio, stock);
         listado_productos.push(producto);
-    
+        console.log(listado_productos);
+        
+        let cantidad = listado_productos.length;
+        cantidad_productos.innerHTML = cantidad;
+        producto.id = cantidad;
         productosJSON = JSON.stringify(listado_productos);
-        console.log(productosJSON);
-        div_productos.innerHTML += `<ul class='productos__lista'>
+
+        div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
                                         <li class='productos__item'>NOMBRE: ${nombre}</li>
                                         <li class='productos__item'>PRECIO: ${precio}</li>
                                         <li class='productos__item'>STOCK: ${stock}</li>
+                                        <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
                                    </ul>`
-    
+        
         localStorage.setItem('productos', productosJSON);
         productos_guardados = JSON.parse(localStorage.getItem('productos'));
         console.log(productos_guardados);
-        cantidad_productos.innerHTML = productos_guardados.length;
-        console.log(productos_guardados.length);
-
+        
+        
 }
 
-//FALTA AGREGAR UN BOTON PARA SACAR EL PRODUCTO DE LA LISTA Y DEL LOCALSTORAGE
+//BORRAR PRODUCTO DEL LOCALSTORAGE Y DEL HTML
+function borrarProducto(id){
+    let idBorrable = document.getElementById(id);
+    idBorrable.remove();
+    let index = listado_productos.indexOf(idBorrable);
+    console.log(listado_productos);
+    console.log(listado_productos[id - 1]);
+    listado_productos.splice(index, 1);
+    console.log(listado_productos);
+    cantidad_productos.innerHTML -= 1;
+    let productosJSON = JSON.stringify(listado_productos);
+    localStorage.setItem('productos', productosJSON);
+}
+
+//VALIDAR TIPO DE DATO NUMERICO Y MAYOR A 0 INPUT DE PRECIO Y STOCK
+let inputPrecio = document.getElementById('precio').addEventListener('input', validarPrecioStock);
+let inputStock = document.getElementById('stock').addEventListener('input', validarPrecioStock);
+
+function validarPrecioStock(e){
+    console.log(typeof e.target.value);
+    let numero = parseInt(e.target.value);
+    console.log(numero);
+
+    if(typeof numero == 'number' && numero > 0){
+        e.target.classList.add('validado');
+        e.target.classList.remove('invalidado');
+        console.log('hola');
+    }else{
+        e.target.classList.add('invalidado')
+        e.target.classList.remove('validado')
+    }
+}
