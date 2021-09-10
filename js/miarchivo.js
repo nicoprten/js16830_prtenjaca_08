1c621f7dc8ad045a5050fba45993e149af7dfd6f
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2021-09-09 22:28:34 -0300

    Pre-entrega 02
    Agregue botón para borrar todos los productos y mejoré un poco el codigo (usaba dos variables para darle la misma función)

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,4 +1,4 @@
-//DESAFÍO DOM CLASE 08
+//PRE-ENTREGA 02 - AGREGUE BOTON PARA BORRAR TODO EL STOCK DE PRODUCTOS.
 
 class Productos{
     constructor(id, nombre, precio, stock){
@@ -15,12 +15,10 @@ let cantidad_productos = document.getElementById('cantidad_productos');
 let productosJSON;
 //ESTUVE MUCHO TIEMPO VIENDO COMO HACER PARA TRAER LO GUARDADO EN EL LOCALSTORAGE, SE ME PISABAN LAS VARIABLES AL TOCAR F5 POR QUE LAS VOLVIA A DECLARAR, LES GUARDE EL VALOR DEL LOCALSTORAGE, SI NO HAY NADA EL VALOR ES NULL, EN ESE CASO SI DECLARAR LAS VARIALBES.
 let productos_guardados = JSON.parse(localStorage.getItem('productos'));
-let listado_productos = JSON.parse(localStorage.getItem('productos'));;
 if(productos_guardados == null){
-    listado_productos = [];
+    productos_guardados = [];
     cantidad_productos.innerHTML = 0;
 }else{
-    console.log(listado_productos);
     cantidad_productos.innerHTML = productos_guardados.length;
     for(producto of productos_guardados){
         div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
@@ -36,13 +34,13 @@ function agregarProducto(){
         let precio = document.getElementById('precio').value;
         let stock = document.getElementById('stock').value;
         let producto = new Productos(0 ,nombre, precio, stock);
-        listado_productos.push(producto);
-        console.log(listado_productos);
+        productos_guardados.push(producto);
+        console.log(productos_guardados);
         
-        let cantidad = listado_productos.length;
+        let cantidad = productos_guardados.length;
         cantidad_productos.innerHTML = cantidad;
         producto.id = cantidad;
-        productosJSON = JSON.stringify(listado_productos);
+        productosJSON = JSON.stringify(productos_guardados);
 
         div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
                                         <li class='productos__item'>NOMBRE: ${nombre}</li>
@@ -51,10 +49,7 @@ function agregarProducto(){
                                         <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
                                    </ul>`
         
-        localStorage.setItem('productos', productosJSON);
-        productos_guardados = JSON.parse(localStorage.getItem('productos'));
-        console.log(productos_guardados);
-        
+        localStorage.setItem('productos', productosJSON);     
         
 }
 
@@ -62,13 +57,10 @@ function agregarProducto(){
 function borrarProducto(id){
     let idBorrable = document.getElementById(id);
     idBorrable.remove();
-    let index = listado_productos.indexOf(idBorrable);
-    console.log(listado_productos);
-    console.log(listado_productos[id - 1]);
-    listado_productos.splice(index, 1);
-    console.log(listado_productos);
+    let index = id - 1;
+    productos_guardados.splice(index, 1);
     cantidad_productos.innerHTML -= 1;
-    let productosJSON = JSON.stringify(listado_productos);
+    let productosJSON = JSON.stringify(productos_guardados);
     localStorage.setItem('productos', productosJSON);
 }
 
@@ -77,16 +69,31 @@ let inputPrecio = document.getElementById('precio').addEventListener('input', va
 let inputStock = document.getElementById('stock').addEventListener('input', validarPrecioStock);
 
 function validarPrecioStock(e){
-    console.log(typeof e.target.value);
     let numero = parseInt(e.target.value);
-    console.log(numero);
 
     if(typeof numero == 'number' && numero > 0){
         e.target.classList.add('validado');
         e.target.classList.remove('invalidado');
-        console.log('hola');
     }else{
         e.target.classList.add('invalidado')
         e.target.classList.remove('validado')
+    }
+}
+
+//FUNCION PARA BORRAR TODO EL STOCK DE PRODUCTOS AGREGADOS
+const borrarTodo = (array) => {
+    // array.forEach(producto => {
+        //     producto.nombre = '';
+        // });
+    console.log('Borrando..');
+    const confirmando = confirm('¿Estás seguro de querer borrar todo?');
+    if(confirmando){
+        array.length = 0; //PARA BORRAR UN ARRAY (NO SÉ SI ES EL MÉTODO ADECUADO)
+        console.log(array);
+        localStorage.clear();
+        div_productos.innerHTML = `<p>PRODUCTOS ELIMINADOS</p>`;
+        cantidad_productos.innerHTML = 0;
+    }else{
+        console.log('Cancelando..');
     }
 }
