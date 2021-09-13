5ef5f9dc072643071f8cec4557773f0bb19d99be
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2021-09-13 17:50:10 -0300

    Add JQuery y mejoré un poco el código

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,5 +1,3 @@
-//PRE-ENTREGA 02 - AGREGUE BOTON PARA BORRAR TODO EL STOCK DE PRODUCTOS.
-
 class Productos{
     constructor(id, nombre, precio, stock){
         this.id = id; 
@@ -11,23 +9,27 @@ class Productos{
 
 let div_productos = document.getElementById('productos');
 let cantidad_productos = document.getElementById('cantidad_productos');
-
 let productosJSON;
-//ESTUVE MUCHO TIEMPO VIENDO COMO HACER PARA TRAER LO GUARDADO EN EL LOCALSTORAGE, SE ME PISABAN LAS VARIABLES AL TOCAR F5 POR QUE LAS VOLVIA A DECLARAR, LES GUARDE EL VALOR DEL LOCALSTORAGE, SI NO HAY NADA EL VALOR ES NULL, EN ESE CASO SI DECLARAR LAS VARIALBES.
 let productos_guardados = JSON.parse(localStorage.getItem('productos'));
 habilitarBorrarTodo();
+function agregarListadoDeProductos(producto){
+    console.log(producto);
+    div_productos.innerHTML += `
+    <ul id='${producto.id}' class='productos__lista'>
+        <li class='productos__item'>NOMBRE: ${producto.nombre}</li>
+        <li class='productos__item'>PRECIO: ${producto.precio}</li>
+        <li class='productos__item'>STOCK: ${producto.stock}</li>
+        <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
+    </ul>`;
+}
+//SI EL LOCALSTORAGE ESTA VACIO == NULL:
 if(productos_guardados == null){
     productos_guardados = [];
     cantidad_productos.innerHTML = 0;
 }else{
     cantidad_productos.innerHTML = productos_guardados.length;
     for(producto of productos_guardados){
-        div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
-                                            <li class='productos__item'>NOMBRE: ${producto.nombre}</li>
-                                            <li class='productos__item'>PRECIO: ${producto.precio}</li>
-                                            <li class='productos__item'>STOCK: ${producto.stock}</li>
-                                            <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
-                                       </ul>`
+        agregarListadoDeProductos(producto);
     }
 }
 function agregarProducto(){
@@ -36,38 +38,22 @@ function agregarProducto(){
         let stock = document.getElementById('stock').value;
         let producto = new Productos(0 ,nombre, precio, stock);
         productos_guardados.push(producto);
-        console.log(productos_guardados);
         habilitarBorrarTodo();
         let cantidad = productos_guardados.length;
         cantidad_productos.innerHTML = cantidad;
         producto.id = cantidad;
         productosJSON = JSON.stringify(productos_guardados);
-
-        div_productos.innerHTML += `<ul id='${producto.id}' class='productos__lista'>
-                                        <li class='productos__item'>NOMBRE: ${nombre}</li>
-                                        <li class='productos__item'>PRECIO: ${precio}</li>
-                                        <li class='productos__item'>STOCK: ${stock}</li>
-                                        <li class='productos__item'><button class='productos__button' onclick='borrarProducto(${producto.id})'>BORRAR</button></li>
-                                   </ul>`
-        
-        localStorage.setItem('productos', productosJSON);     
-        
+        localStorage.setItem('productos', productosJSON);   
+        agregarListadoDeProductos(producto);
 }
 
 // BORRAR PRODUCTO DEL LOCALSTORAGE Y DEL HTML
-// DATO: TRATE DE BORRARLOS CON SPLICE, RESTANDOLE 1 AL ID SACABA EL INDICE, PERO SE ROMPIA AL BORRAR VARIOS ELEMENTOS Y ROMPER EL ORDEN DE PRODUCTO.ID
-// POR ESO PASE A USAR EL METODO .FILTER
 function borrarProducto(id){
-    
     let idBorrable = document.getElementById(id);
     idBorrable.remove();
-    // console.log(idBorrable.id);
-    // let index = id - 1;
-    // console.log(index);
+    cantidad_productos.innerHTML -= 1;
     productos_guardados = productos_guardados.filter(producto => producto.id != id);
     habilitarBorrarTodo();
-    // productos_guardados.splice(index, 1);
-    cantidad_productos.innerHTML -= 1;
     let productosJSON = JSON.stringify(productos_guardados);
     localStorage.setItem('productos', productosJSON);
 }
@@ -75,11 +61,16 @@ function borrarProducto(id){
 //VALIDAR TIPO DE DATO NUMERICO Y MAYOR A 0 INPUT DE PRECIO Y STOCK
 let inputPrecio = document.getElementById('precio').addEventListener('input', validarPrecioStock);
 let inputStock = document.getElementById('stock').addEventListener('input', validarPrecioStock);
+// achicando el codigo con JQuery:
+let inputs = $('.form__input').on('keyup', (e) => {
+    if(e.keyCode === 13){
+            agregarProducto();
+    }
+});
 
 function validarPrecioStock(event){
     let numero = parseInt(event.target.value);
-
-    if(typeof numero == 'number' && numero > 0){
+    if(numero > 0){
         event.target.classList.add('validado');
         event.target.classList.remove('invalidado');
     }else{
